d3db0dbe520b1b28d61fab3d3dd66d6b88e5376b
John D. Rowell <me@jdrowell.com>
2009-06-28 22:13:39 -0300

    Trying to deploy to Heroku with full db.

---
+++ config.ru
@@ -0,0 +1,4 @@
+require 'geoip'
+
+run Sinatra::Application
+
--- geoip.rb
+++ geoip.rb
@@ -1,19 +1,29 @@
 require 'rubygems'
 require 'sinatra'
+#require 'ruby-debug/debugger'
 
 require 'geoip_city'
 require 'yajl'
+require 'iconv'
+
+mime :json, 'application/json'
 
 class Provider
   @@geodb = GeoIPCity::Database.new("GeoLiteCity.dat", :filesystem)
+  @@to_charset = 'utf8'
+  @@from_charset = 'iso-8859-1'
 
   def self.resolve(ip)
-    @@geodb.look_up(ip) || { :country_code => nil }
+    res = @@geodb.look_up(ip) || { :country_code => nil }
+    [:city, :country_name].each do |field|
+      res[field] = Iconv.conv(@@to_charset, @@from_charset, res[field])
+    end
+    res
   end
 end
     
 get '/geoip/:ip' do |ip|
-  response['Content-Type'] = 'application-json'
+  content_type :json
   Yajl::Encoder.encode(Provider.resolve(ip))
 end
 
